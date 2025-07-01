55dbe098000d8832a1667783bc74d6e5a4e72d14
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2025-07-01 16:34:31 -0600

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,17 +1,26 @@
 let handler = m => m
+
 handler.all = async function (m) {
-let setting = global.db.data.settings[this.user.jid]
-	
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 2000) }) * 1000}
-let uptime = clockString(_uptime)
-let bio = `${global.packname} ║ ✅ ${uptime} ⌛ ║ @ceogeripium.dzn`
-await this.updateProfileStatus(bio).catch(_ => _)
-setting.status = new Date() * 1
-} 
+  let setting = global.db.data.settings[this.user.jid]
+
+  let _uptime = process.uptime() * 1000
+  let _muptime
+  if (process.send) {
+    process.send('uptime')
+    _muptime = await new Promise(resolve => {
+      process.once('message', resolve)
+      setTimeout(resolve, 2000)
+    }) * 1000
+  }
+
+  let uptime = clockString(_uptime)
+
+  let bio = `🤖 Los Tazos Dorados 🥇 | ⏱️ Activo: ${uptime} | 🔥 León Dorado 🦁`
+  await this.updateProfileStatus(bio).catch(_ => _)
+
+  setting.status = new Date() * 1
+}
+
 export default handler
 
 function clockString(ms) {
@@ -19,5 +28,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('') 
-} 
+  return [d + 'd', h + 'h', m + 'm', s + 's'].join(' ')
+}
